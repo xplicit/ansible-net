6f043597eeace36cb30400d4c3d30d2a41cc69a0
xplicit <svg@ngs.ru>
2018-05-05 20:59:42 +0700

    Add template for future unit tests

--- tests/ansible-net.tests/UnitTest1.cs
+++ tests/ansible-net.tests/UnitTest1.cs
@@ -15,13 +15,27 @@ namespace Ansible.Tests
         [Test]
         public void CheckVersion()
         {
-            var sb = new StringBuilder();            
+            var sb = new StringBuilder();
 
             var playbook = new Playbook("test").Version();
             
             playbook.Execute(x => sb.AppendLine(x));
 
             Assert.That(sb.ToString(), Does.Contain("ansible-playbook"));
+        }
+
+        [Test]
+        public void CheckOutput()
+        {
+            var sb = new StringBuilder();
+
+            var playbook = new Playbook("../../../../playbooks/environment.yml");
+            
+            playbook.Execute(x => sb.AppendLine(x));
+
+            var t=sb.ToString();
+
+            Assert.Pass();
         }
     }
 }
